6aed3d8cd764140805e03cb1082734586a772621
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2023-02-23 19:17:23 +0530

    Create jokes.js

---
+++ jokes.js
@@ -0,0 +1,11 @@
+ar=['Riddhi: I ❤️ JavaScript.','Mohit: But, this is horrible.','programmer : 0=="0"','JS : true','programmer: 0==[]','JS :true','programmer : Ok so "0"==[] right? Makes sense to me..','JS : false','programmer : GET OUT!..','q. Why was the JavaScript developer sad? a. Because they didn','t know what state it was in!','q. Why did the React Higher Order Component give up? a. Because it sur-rendered to the prop-aganda!','q. Why did the react class component feel relieved? a. Because it was now off the hook.','q. Why couldn’t the React component understand the joke? a. Because it didn’t get the context.','Q: Why did the jQuery developer never have financial problems?','A: Because he was in $.noConflict() mode','Q: What did Java say to his son?','A: You']
+            l=ar.length
+            console.log(l)
+            function jks(){
+                x=Math.floor(Math.random()*l)
+                jk=ar[x]
+                console.log(x)
+            document.getElementById("joke").innerHTML=jk
+            console.log(jk)}
+            setInterval(jks,10000)
+            console.log(jk)
